63c8b1a6c82dda0051b9a648adaaafc1fe3e1587
asmita <asmita.singh1710@gmil.com>
2022-03-23 01:23:41 +0530

    checkin files

---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note (){
+    return (<div className="note">
+        <h1 contenteditable="true">Heading</h1>
+        <p contenteditable="true">Type the note</p>
+    </div>);
+}
+
+
+export default Note;
